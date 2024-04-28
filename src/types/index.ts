1f1259ccc3756cf0ca19c22301ce8f5e6b2511f6
Andrey Kozhuhov <andrpre96@gmail.com>
2024-04-28 16:02:10 +0300

    Start

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+export interface IProductItem {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+export type IBasketItem = Pick<IProductItem, 'id' | 'title' | 'price'>;
+
+export interface IAppState {
+    catalog: IProductItem[];
+    basket: string[];
+    preview: string | null;
+    order: IOrder | null;
+    loading: boolean;
+}
+
+export interface IOrderForm {
+    payment: 'Онлайн' | 'При получении';
+    address: string;
+    email: string;
+    phone: string;
+}
+
+export interface IOrder extends IOrderForm {
+    items: string[]
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface IOrderResult {
+    id: string;
+    total: number;
+}
